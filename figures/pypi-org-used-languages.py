1d57e0ce7d4fdd7942c5c6a8034ae6fd3eaa5749
Jonas Pleyer <jonas.dev@pleyer.org>
2025-08-22 19:24:45 +0200

    add figures for paper

---
+++ figures/pypi-org-used-languages.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+
+
+def prepare_fig_ax():
+    plt.rcParams.update(
+        {
+            "font.family": "Courier New",  # monospace font
+            "font.size": 20,
+            "axes.titlesize": 20,
+            "axes.labelsize": 20,
+            "xtick.labelsize": 20,
+            "ytick.labelsize": 20,
+            "legend.fontsize": 20,
+            "figure.titlesize": 20,
+        }
+    )
+    fig, ax = plt.subplots(figsize=(8, 6))
+    fig.subplots_adjust(
+        top=0.9,
+        left=0.2,
+        right=0.8,
+        bottom=0.15,
+    )
+    ax.grid(True, which="major", linestyle="-", linewidth=0.75, alpha=0.25)
+    ax.minorticks_off()
+    ax.grid(True, which="minor", linestyle="-", linewidth=0.25, alpha=0.15)
+    ax.set_axisbelow(True)
+    return fig, ax
+
+
+data = [
+    ("Go", 20 * 3, "#00acd7"),
+    ("Java", 20 * 10, "#c12f30"),
+    ("Fortran", 20 * 13, "#744e97"),
+    ("C", 20 * 101, "#659bd3"),
+    ("C++", 20 * 119, "#00599d"),
+    ("Rust", 20 * 158, "#CE412B"),
+]
+
+fig, ax = prepare_fig_ax()
+
+ax.barh(
+    [d[0] for d in data],
+    [d[1] for d in data],
+    color=[d[2] for d in data],
+    align="center",
+)
+ax.set_xlabel("(B) Number of Packages")
+ax.set_title("Packages at pypi.org")
+
+fig.savefig("figures/pypi-org-used-languages.png")
+fig.savefig("figures/pypi-org-used-languages.pdf")
+fig.savefig("figures/pypi-org-used-languages.svg")
---
+++ figures/stackoverflow-popular-languages.py
@@ -0,0 +1,62 @@
+import matplotlib as mpl
+import importlib
+
+pypi_mod = importlib.import_module("pypi-org-used-languages", package=None)
+
+fig, ax = pypi_mod.prepare_fig_ax()
+
+groups = ["Learners", "Professionals", "Total"]
+data_individual = [
+    ("Fortran", [2, 0.9, 1.4], "#744e97"),
+    ("Rust", [23.1, 14.5, 14.8], "#CE412B"),
+    ("Go", [13.1, 17.4, 16.4], "#00acd7"),
+    ("C", [48, 19.1, 22], "#659bd3"),
+    ("C++", [44.6, 21.8, 23.5], "#00599d"),
+    ("Java", [40.8, 29.6, 29.4], "#c12f30"),
+]
+
+data = []
+for d in data_individual:
+    for n, group in enumerate(groups):
+        color = mpl.colors.to_rgba(d[2], alpha=(n + 1) / len(groups))
+        data.append((f"{d[0]} ({group})", d[1][n], color))
+
+labels = [
+    "" if i % 3 != 1 else data_individual[int((i - 1) / 3)][0]
+    for i, d in enumerate(data)
+]
+ax.barh(
+    [d[0] for d in data],
+    [d[1] for d in data],
+    color=[mpl.colors.to_rgba(d[2]) for d in data],
+    label=labels,
+)
+ax.set_yticklabels(labels)  # , horizontalalignment="left")
+
+# handles, labels = ax.get_legend_handles_labels()
+# print(handles[0])
+handles = [
+    mpl.patches.Rectangle(
+        xy=(0, 0.6),
+        width=0.9,
+        height=0.8,
+        angle=0,
+        facecolor=mpl.colors.to_rgba("#000", alpha=(len(groups) - i) / len(groups)),
+    )
+    for i in range(len(groups))
+]
+ax.legend(
+    handles,
+    groups[::-1],
+    ncol=3,
+    frameon=False,
+    loc="upper center",
+    bbox_to_anchor=(0.5, 1.15),
+)
+
+# ax.set_title("Language Popularity (stackoverflow.co)")
+ax.set_xlabel("(A) Language Usage [%]")
+
+fig.savefig("figures/stackoverflow-popular-languages.png")
+fig.savefig("figures/stackoverflow-popular-languages.pdf")
+fig.savefig("figures/stackoverflow-popular-languages.svg")
